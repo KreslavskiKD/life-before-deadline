167bcffa81b938c83b63b82c5fd54bc170ee70e7
KKD <kirill.kreslavski@gmail.com>
2022-10-19 15:21:09 +0300

    UI base for project

---
+++ app/src/main/java/com/example/lifebeforedeadline/HomeActivity.kt
@@ -0,0 +1,23 @@
+package com.example.lifebeforedeadline
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lifebeforedeadline.databinding.HomeActivityBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = HomeActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.activityToolbar)
+        handleClicks(binding)
+    }
+
+    private fun handleClicks(binding: HomeActivityBinding) {
+        binding.calendarViewSample.setOnClickListener {
+            startActivity(Intent(this, CalendarViewActivity::class.java))
+        }
+    }
+}
